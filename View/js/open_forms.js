78f99e2e414b5083c3a756220e7ec7d5bb15553b
YanisISEP <45003720+YanisISEP@users.noreply.github.com>
2020-05-09 06:03:16 +0200

    Update for article pop-up
    Ouvre et ferme les popup article (ainsi que le closer qui englobe tous les articles) en fonction des appels de fonctions

--- View/js/open_forms.js
+++ View/js/open_forms.js
@@ -15,3 +15,32 @@ function openRegisterForm() {
 function closeRegisterForm() {
     document.getElementById("registerForm").style.display = "none";
 }
+
+function openArticle(id) {
+    const y = document.getElementsByClassName("article-popup");
+    const x = document.getElementsByClassName("article-popup-closer");
+
+    let i;
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = 'none';
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = 'block';
+    }
+    document.getElementById(id).style.display = "block";
+    window.location.href = '#main';
+    x[0].style.display = 'block';
+
+}
+
+function closeArticle() {
+    const x = document.getElementsByClassName("article-popup-closer");
+    const y = document.getElementsByClassName("article-popup");
+    let i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = 'none';
+    }
+    for (i = 0; i < y.length; i++) {
+        y[i].style.display = 'none';
+    }
+}
