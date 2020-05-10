af8e0a2c06a79891eb9f7731859362a788d7e1b4
YanisISEP <45003720+YanisISEP@users.noreply.github.com>
2020-05-10 13:44:45 +0200

    comment addUser JS
    Pas utile, plus puissant en PHP

--- View/js/include_header_footer.js
+++ View/js/include_header_footer.js
@@ -8,11 +8,11 @@ function addGlobalFooter() {
         $("#globalFooter").load("global_footer.html");
     });
 }
-function addUserHeader() {
-    $(function () {
-        $("#userHeader").load("user_header.php");
-    });
-}
+// function addUserHeader() {
+//     $(function () {
+//         $("#userHeader").load("user_header.php");
+//     });
+// }
 function addUserFooter() {
     $(function () {
         $("#userFooter").load("user_footer.html");
