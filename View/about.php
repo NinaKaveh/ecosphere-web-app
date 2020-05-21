f0b10783d17d90a86f125f9af4c126e37395a6a3
Nina <nina.kaveh1998@gmail.com>
2020-05-21 21:34:22 +0200

    Add content to frontend

--- View/about.php
+++ View/about.php
@@ -14,59 +14,40 @@
 </header>
 
 <main class="panel">
-    <h1>EcoSphere</h1>
+    <h1>Ecosphere</h1>
     <hr/>
     <blockquote>
         <p>
-            Ecosphere is a website, more exactly a blog. It should allow registered users to publish articles related to
-            the environment.
-            These articles can be published by professionals as well as individuals.
-            Their reading will be open to registered members of the ecosphere blog as well as visitors to the website.
-            The aim is to allow people to take an interest in the environment by reading articles for free and thus,
-            perhaps, to have the desire to publish an article on this blog to defend their point of view on the
-            environment.
-            This website is therefore a way to defend these convictions as well as to share with as many people as
-            possible the beauties of this world that are in danger today.
+            This is the participative platform, Ecosphere, with which students and employees of ISEP, a digital
+            engineering school, will be able to follow environmental news and hence play a role, at their own level, in
+            a context of ecological transition.<br>
+            This platform can be accessed only by registered members. So, you need to be a user of the website to be
+            able to read/publish articles, and participate to the XP system.<br>
+            The aim is to allow people to take an interest in the ecology by reading articles for free and thus,
+            perhaps, to long for publishing articles on this blog to stand for their point of view on the environment.
+            This website is therefore a way to stand up for these convictions as well as to share with as many people as
+            possible the beauties of this world that are in danger today.<br>
+            The aim is that the platform will be entertaining, by letting members contribute to the flow of information
+            themselves, while challenging its members with a system of user XP for each relevant publication or
+            like to one of their articles.
         </p>
     </blockquote>
 
     <h3>The history of the team</h3>
     <blockquote>
         <p>
-            Our team was born in 2019. It was created at the same time as the EcoSphere project.
-            It brings together three students from ISEP, a digital engineering school.
-            This project is then a study project in BAC+4 in the field of IS architecture.
-        </p>
-
-    </blockquote>
-
-
-    <h3>Yanis KETTOU</h3>
-    <blockquote>
-        <p>
-            Yanis is a student of cybersecurity courses.
-        </p>
-    </blockquote>
-
-    <h3>Nina KAVEH</h3>
-    <blockquote>
-        <p>
-            Nina is a student of SI courses.
-        </p>
-    </blockquote>
-
-    <h3>Julie CAUDROIT</h3>
-    <blockquote>
-        <p>
-            Julie is a student of cybersecurity courses.
+            Our group 'The Witcher' has its origins in a common attachment to ecology. This subject is important to us
+            and we would like to develop a concrete and innovative project illustrating this topic.
+            We are a team of three ISEP students. This platform is a challenge for us, allowing us to show our skills in
+            design, development and deployment of web applications.
         </p>
     </blockquote>
 
     <h3>The team</h3>
     <blockquote>
         <p>
-            Each person's two educational backgrounds allow the team to carry out this project at the same time by
-            advancing with each person's knowledge.
+            Each person has two educational backgrounds allowing the team to carry out this project at the same time by
+            advancing with each one's knowledge.<br>
             Although some are more at ease with programming, each one tries at his own level to make the project viable
             and deliver a website with high expectations.
         </p>
--- View/help.php
+++ View/help.php
@@ -15,6 +15,15 @@
 
 <main>
     <article class="panel">
+        <h3 class="question"> How to register ? </h3>
+        <p class="reponse">
+            If you have never used the platform, you can register by going back to the home page and clicking on the
+            <i>register</i>button. Then you need to fill the form. Make sure you are not misspelling your email or
+            password. In case of error when you register, see section "Who to contact in case of problems ?"
+        </p>
+    </article>
+
+    <article class="panel">
         <h3 class="question"> How do I login ? </h3>
         <p class="reponse">
             You go to the button at the top marked "login". By pressing it, you will be asked to enter your information
@@ -25,14 +34,86 @@
     </article>
 
     <article class="panel">
-        <h3 class="question"> How to post an article ? </h3>
+        <h3 class="question"> Who to contact in case of problems ? </h3>
         <p class="reponse">
-            First, it is necessary to be a member of Ecosphere and to connect to your account.
-            Then, to publish an article, you just have to go to the article page and write your article by clicking on
-            the button.
-            We will then let you write it before it is published on the site and visible to the community.
+            If you have any problem with the platform (forget your password, unpublished article...), you can send a
+            mail to <a class="redirection" href="mailto:support@ecosphere.com">support@ecosphere.com</a>. You will be
+            recontacted soon to reset your password. You can also call the support service : 01 02 03 04 05, available
+            from Monday to Saturday - 8:00am to 6:00pm (0.30€ per minute + the price of a call).
         </p>
     </article>
+
+    <article class="panel">
+        <h3 class="question"> How to post an article ? </h3>
+        <p class="reponse">
+            First, it is necessary to be a member of ecosphere and to connect to your account.
+            Then, to publish an article, you just have to go to the article page and write your article by clicking on
+            the button. You will then write it before submitting it on the site and make it visible to the community.
+        </p>
+    </article>
+
+    <article class="panel">
+        <h3 class="question"> What can I publish on the Ecosphere platform ? </h3>
+        <p class="reponse">
+            Ecosphere is a participative platform with which users will be able to follow environmental news by reading
+            of publishing ecological content. Any other content not related to ecology, sustainable development... can
+            be deleted from the platform.
+        </p>
+    </article>
+
+    <article class="panel">
+        <h3 class="question"> How do I report bad behavior ? </h3>
+        <p class="reponse">
+            If you notice a user having bad behavior, you can contact the support team (see section "Who to contact in
+            case of problems ?"). The following cases are considered as bad behavior:
+        </p>
+        <ul>
+            <li>Plagiat</li>
+            <li>Advertising</li>
+            <li>Discrimination</li>
+            <li>Hate content</li>
+            <li>Paedophile or pornographic content</li>
+            <li>Unrelevent content (not connected to ecology)</li>
+        </ul>
+    </article>
+
+    <article class="panel">
+        <h3 class="question"> How does the "like" system works ? </h3>
+        <p class="reponse">
+            When you publish an article, the other users can read it. You may then add an appreciation, a "like" that
+            will send xp to the author of the article. This system is for all users. If you publish an article, people
+            can like it.
+        </p>
+    </article>
+
+    <article class="panel">
+        <h3 class="question"> How to win xp on our profile ? </h3>
+        <p class="reponse">
+            The xp system is linked to the article system. Indeed, there a two ways to win xp. The first is by
+            publishing articles (they need to be relevant otherwise an admin will delete it and you will receive no
+            points). Then, once the article is published, people adding a like to your article will make you win some
+            xp. In other way, the best way to have a lot of xp is to be active on the platform and to publish articles
+            that other users will like.
+        </p>
+    </article>
+
+    <article class="panel">
+        <h3 class="question"> How are the data regarding coronavirus collected ? </h3>
+        <p class="reponse">
+            The data collected are half stored, half retrieved from another API. If you click on the "COVID-19" button
+            in the navigation bar, you will be redirected to the section summing up all the date from March 1st to May
+            20th. These data are stored our database and their purpose is to show the evolution of the pandemic is some
+            affected countries. The other type of data is coming from the website
+            <a href="https://coronavirus-19-api.herokuapp.com/countries/" class="redirection">coronavirus-19-api.herokuapp.com</a>
+            where there are only the data of today and are updated daily.
+        </p>
+    </article>
+
+    <article class="panel">
+        <h3 class="question"> Is the website secured ? </h3>
+        <p class="reponse">Yes it is don't worry !</p>
+    </article>
+
 
     <footer>
         <div id="globalFooter">
