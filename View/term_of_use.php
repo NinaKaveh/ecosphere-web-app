daa7030400fc01e6c9c8264e44278d48767df3af
YanisISEP <45003720+YanisISEP@users.noreply.github.com>
2020-05-27 21:37:34 +0200

    Update term_of_use.php
    Acces avec user possible

--- View/term_of_use.php
+++ View/term_of_use.php
@@ -9,13 +9,35 @@
 </head>
 
 <body>
-<header>
-    <div id="globalHeader">
-        <script>
-            addGlobalHeader();
-        </script>
-    </div>
-</header>
+<?php session_start();
+if(!empty($_SESSION['name'])) {
+    ?>
+    <header>
+        <div id="header">
+            <img id="ecosphere_logo" src="img/logo-paysage.png" alt="logo" width="195"/>
+            <img id="isep_logo" src="img/logo-isep.png" alt="logo" width="60"/>
+
+            <div id="banner">
+                <nav class="navbar">
+                    <a href="user/user_homepage.php">Home</a>
+                    <a href="user/user_profile.php">Profile</a>
+                    <a href="user/all_articles.php">All articles</a>
+                    <a href="covid.php">COVID-19</a>
+                    <span>Hello <?php echo $_SESSION['name'];?></span><button class="//button" onclick="window.location.href='user/logout.php'">Logout</button>
+                </nav>
+
+            </div>
+
+
+        </div>
+    </header>
+<?php } else { ?>
+    <header>
+        <div id="globalHeader">
+            <script>addGlobalHeader();</script>
+        </div>
+    </header>
+<?php } ?>
 <main class="panel">
     <div>
         <h1>Terms of use</h1>
